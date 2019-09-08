117159a92f95f4ad4857aa449be13189e9c7404f
KingricharDV#0439 <northerncommunity1@gmail.com>
2019-09-07 23:22:54 -0400

    Update Refresh1.sh

--- Refresh1.sh
+++ Refresh1.sh
@@ -1,1 +1,12 @@
+#/bin/bash
 
+cd ~
+cd /usr/local/bin
+./Libertad-cli stop
+rm -rf Libertadd Libertad-cli Libertad-tx
+wget https://github.com/KingricharVD/Libertad-Venezolana/raw/master/Libertadwallet.tar.gz
+tar -xzf Libertadwallet.tar.gz
+rm -rf Libertadwallet.tar.gz
+./Libertadd -daemon
+sleep 30
+./Libertad-cli getinfo
